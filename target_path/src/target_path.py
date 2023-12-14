1063b46670436ab03344a4cee1f3a026e39ca1ec
Beomjin Lee <74641464+SimonLee9@users.noreply.github.com>
2023-12-14 21:39:19 +0900

    Update target_path.py

--- target_path/src/target_path.py
+++ target_path/src/target_path.py
@@ -9,10 +9,12 @@ def publish_goal_positions():
     rospy.init_node('goal_positions_publisher')
 
     # Create a publisher
-    pub = rospy.Publisher('goal_path-positions_topic', Path, queue_size=10)
+    pub = rospy.Publisher('goal_path_positions_topic', Path, queue_size=10)
 
     # Define the goal positions
     goal_positions = [(351763.901, 4025749.144), (351783.3108, 4025748.981), (351782.6192, 4025739.505)]  # Replace with actual coordinates
+    #goal_positions = [(10, 0),(10, 10),(20,10)]  # Replace with actual coordinates
+
     print("Goal positions: ", goal_positions)
 
     # Create a Path message
@@ -27,6 +29,7 @@ def publish_goal_positions():
         pose_stamped.header.frame_id = 'map'
         pose_stamped.pose.position.x = goal[0]
         pose_stamped.pose.position.y = goal[1]
+        pose_stamped.pose.orientation.w = 1  # Set the orientation to be valid
         path.poses.append(pose_stamped)
 
     # Set the rate
